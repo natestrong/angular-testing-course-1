3daca63fc20ed2c012197ca283ffea01a818fd4a
nstrong <nathanstrong.fx@gmail.com>
2021-07-08 08:44:51 -0700

    init

--- src/app/courses/services/calculator.service.ts
+++ src/app/courses/services/calculator.service.ts
@@ -6,19 +6,15 @@ import {LoggerService} from './logger.service';
   providedIn: 'root'
 })
 export class CalculatorService {
+  constructor(private logger: LoggerService) {}
 
-
-  constructor(private logger: LoggerService) {
-
-  }
-
-  add(n1: number, n2:number) {
-    this.logger.log("Addition operation called");
+  add(n1: number, n2: number) {
+    this.logger.log('Addition operation called');
     return n1 + n2;
   }
 
-  subtract(n1: number, n2:number) {
-    this.logger.log("Subtraction operation called");
+  subtract(n1: number, n2: number) {
+    this.logger.log('Subtraction operation called');
     return n1 - n2;
   }
 
